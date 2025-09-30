ec3433cf342f92ef2501b71c443ada58ce464980
2giseok <rltjr147@gmail.com>
2025-09-30 16:18:11 +0900

    photospot

---
+++ src/main/java/me/leegiseok/photospot/PhotospotApplication.java
@@ -0,0 +1,13 @@
+package me.leegiseok.photospot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotospotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotospotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/leegiseok/photospot/PhotospotApplicationTests.java
@@ -0,0 +1,13 @@
+package me.leegiseok.photospot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotospotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
